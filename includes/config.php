66306130bad1d5e8a0d38d4552195104b5c1b2f4
Palak Khatri <74138839+Palakkk@users.noreply.github.com>
2022-11-16 21:30:40 +0530

    Add files via upload

---
+++ includes/config.php
@@ -0,0 +1,22 @@
+<?php
+    /* Heroku ClearDB Database url */
+    // mysql://b8f8d855c93d08:2a552266@us-cdbr-iron-east-02.cleardb.net/heroku_669bd6ce71fcdec?reconnect=true
+    // mysql --host=us-cdbr-iron-east-02.cleardb.net --user=b8f8d855c93d08 --password=2a552266 --reconnect heroku_669bd6ce71fcdec < spotify.sql
+
+
+    ob_start();
+    session_start();
+    
+    $timezone = date_default_timezone_set("Europe/London");
+    $con = mysqli_connect("localhost", "root", "", "musicdb" ); // when using local phpmyadmin database
+    if ($con->connect_error) {
+        die("Connection failed: " . $con->connect_error);
+    }
+
+    if(mysqli_connect_errno()) {
+        echo "Failed to connect" . mysqli_connect_errno();
+    }
+
+
+
+// $con = mysqli_connect("us-cdbr-iron-east-02.cleardb.net", "b8f8d855c93d08", "2a552266", "heroku_669bd6ce71fcdec"); // when using ClearDB database with heroku
